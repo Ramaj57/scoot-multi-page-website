ed167e9ccb786946b56cd1c03fdd419147fa2efb
Ramaj57 <slamaster02@yahoo.com>
2025-04-01 10:00:54 -0400

    all header component code

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonComponent } from "../shared/button/button.component";
+import { ButtonComponent } from '../shared/button/button.component';
+import { NavLinksComponent } from '../nav-links/nav-links.component';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, NavLinksComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
-export class HeaderComponent {
-}
+export class HeaderComponent {}
